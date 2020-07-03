21752ddcca14307c68a14b3c967cb24cca25a3ad
wsb <18133617125@163.com>
2020-07-03 20:38:27 +0800

    更改controller

--- src/main/java/com/wangsb/controller/MyController.java
+++ src/main/java/com/wangsb/controller/MyController.java
@@ -7,11 +7,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class MyController {
     @RequestMapping(value = "/some.do")
-    public ModelAndView doSome(){
+    public ModelAndView doSome() {
         ModelAndView modelAndView = new ModelAndView();
-        modelAndView.addObject("msg","欢迎使用Springmvc");
-        modelAndView.addObject("fun","requestMapping()");
+        modelAndView.addObject("msg", "欢迎使用Springmvc");
+        modelAndView.addObject("fun", "requestMapping()");
         modelAndView.setViewName("show");
+        System.out.println("测试git");
         return modelAndView;
     }
 }
