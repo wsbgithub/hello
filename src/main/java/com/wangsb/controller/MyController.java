e71800f23e9c4503283a5dcfb415237a8e37ca67
wsb <18133617125@163.com>
2020-07-04 08:30:10 +0800

    第二次提交

--- src/main/java/com/wangsb/controller/MyController.java
+++ src/main/java/com/wangsb/controller/MyController.java
@@ -13,6 +13,7 @@ public class MyController {
         modelAndView.addObject("fun", "requestMapping()");
         modelAndView.setViewName("show");
         System.out.println("测试git");
+        System.out.println("第二天测试,我来看看能不能上传到远程服务器");
         return modelAndView;
     }
 }
